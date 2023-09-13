4062fa1606ec3596a0bc51f53a39432b553f17f2
MosheMatabologa <gideonmatabologa@gmail.com>
2023-09-13 05:47:00 -0700

    l

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
-
+* main - Test function for positive or negative
+* Return: 0
+*/
 int main(void)
 {
 int i;
