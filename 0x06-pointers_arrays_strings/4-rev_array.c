dd526027abe2f0a76395b8e2bfd468f9934481f6
MosheMatabologa <gideonmatabologa@gmail.com>
2023-09-21 12:13:35 -0700

    Another run

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,56 @@
-include "main.h"
+#include "main.h"
+#include <stdio.h>
+
 /**
- * reverse_array - reverse array of integers
- * @a: array
- * @n: number of elements of array
+ * main - Entry point
  *
- * Return: void
+ * Description: Test the reverse_array function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int arr[] = {1, 2, 3, 4, 5};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int i;
+
+	printf("Original array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	reverse_array(arr, size);
+
+	printf("Reversed array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
+
+/**
+ * reverse_array - Reverse array of integers.
+ * @a: Array.
+ * @n: Number of elements in the array.
+ *
+ * Description: Reverses the order of elements in an integer array.
+ *
+ * Return: void.
  */
 void reverse_array(int *a, int n)
 {
 	int i;
 	int t;
 
-	for (i = 0; i < n--; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = t;
 	}
 }
