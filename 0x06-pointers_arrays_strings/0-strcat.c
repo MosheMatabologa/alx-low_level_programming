98e83d035872c57eaf65087871a143ab433adc30
MosheMatabologa <gideonmatabologa@gmail.com>
2023-09-21 06:29:52 -0700

    1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,29 @@
 #include "main.h"
-
 /**
- * *_strcat - Entry point
- * @dest: destination string
- * @src: string to append
- * Return: pointer to the resulting string
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
-	j = 0;
-
 	while (dest[i] != '\0')
+	{
 		i++;
-
+	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
 	}
-	dest[i] = '\0';
 
+	dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,28 @@
 #include "main.h"
+
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- *
- * Return: dest
+ * _strncat - Concatenate two strings, using at most 'n' bytes from src.
+ * @dest: Destination string.
+ * @src: Source string.
+ * @n: Number of bytes to append from src.
+ * Return: Pointer to the resulting string 'dest'.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 0;
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
+
 	j = 0;
-	while (j < n && src[j] != '\0')
+	while (src[j] != '\0' && j < n)
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
