8774653335c55c24399f2f8d73b409a6d7b0e66f
MosheMatabologa <gideonmatabologa@gmail.com>
2023-09-21 12:25:20 -0700

    next one

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,3 @@
-104-print_buffer.c
-
 #include "main.h"
 #include <stdio.h>
 
@@ -40,7 +38,7 @@ void print_buffer(char *b, int size)
 		{
 			int c = *(b + o + i);
 
-			if (c < 32 || c > 132)
+			if (c < 32 || c > 126)
 			{
 				c = '.';
 			}
