0abea3eaeb2916d8725406b92f83118d43d45503
MosheMatabologa <gideonmatabologa@gmail.com>
2023-09-21 15:07:57 -0700

    a

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,51 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - Entry point for the program.
+ * reverse_array - Reverses the content of an array of intergers.
+ * @a: The array of intergers to be reversed.
+ * @n: The number of elements of array.
  *
- * Description: This function is the entry point for the program. It initializes
- * an array, prints the original array, reverses it, and prints the reversed array.
- *
- * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int arr[] = {1, 2, 3, 4, 5};
-	int size = sizeof(arr) / sizeof(arr[0]);
-	int i;
 
-	printf("Original array: ");
-	for (i = 0; i < size; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-
-	reverse_array(arr, size);
-
-	printf("Reversed array: ");
-	for (i = 0; i < size; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-
-	return (0);
-}
-
-/**
- * reverse_array - Reverses the content of an array of integers.
- * @a: The array of integers to be reversed.
- * @n: The number of elements in the array.
- */
 void reverse_array(int *a, int n)
-{
-	int tmp, index;
 
-	for (index = n - 1; index >= n / 2; index--)
-	{
-		tmp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = tmp;
-	}
+{
+        
+        int tmp, index;
+
+        for (index = n - 1; index >= n /  2; index--)
+        {
+        tmp =a[n - 1 - index];
+        a[n - 1 - index] = a[index];
+        a[index] = tmp;
+        }
 }
