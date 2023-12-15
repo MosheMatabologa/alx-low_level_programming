42415e71546621fa3dcafcefdc7573693f1ce98e
MosheMatabologa <gideonmatabologa@gmail.com>
2023-12-15 06:18:34 -0800

    done

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -4,23 +4,24 @@
 #include <stdio.h>
 #include <string.h>
 
+int my_strlen(char *s);
+void my_puts(char *s);
+char *my_strcpy(char *dest, char *src);
+char *my_strchr(char *s, char c);
+
 int int_putchar(char c);
 int islower(int c);
 int isalpha(int c);
 int abs(int n);
 int isupper(int c);
 int isdigit(int c);
-int strlen(char *s);
 void puts(char *s);
-char *strcpy(char *dest, char *src);
-int atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 void char_memcpy(char *dest, char *src, unsigned int n);
-char *strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
